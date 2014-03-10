63ecfc71031524135a6434a1857e69fb8075c1ef
benclarkwood <dev@devs-mbp.att.net>
2014-03-10 16:48:39 -0700

    More stuff!

--- js/easel.js
+++ js/easel.js
@@ -5,11 +5,6 @@ function startEasel() {
 		
 		elementCounter += $(this).children(".canvas-element").length;
 	});
-	
-	// Add click handler for the page tabs
-	$(".page-tab").click( function() {
-		pageTabWasClicked(this);
-	});
 
 	// Add click handler for all existing elements
 	$(".canvas-element").click( function() {
@@ -51,9 +46,7 @@ function startEasel() {
  
  function addNewCanvasWithTitle(canvasTitle) {
   	newCanvasId = "canvas-" + canvasCounter;
- 	
- 	newPageTabId = "pagetab-" + canvasCounter;
- 	
+ 	 	
  	// Create the new canvas from the template
  	var newCanvas = $("#canvas-template").clone(true).removeAttr('id');
  	 	 	
@@ -62,17 +55,8 @@ function startEasel() {
  	// Deep copy does not preserve droppable. Instantiate here instead.
  	newCanvas.droppable();
  	
- 	// Create the new pageTab from the template
- 	var newPageTab = $("#page-tab-template").clone(true).removeAttr('id');
- 	
- 	newPageTab.attr('id', newPageTabId);
- 	
- 	// Set the title of the page tab
- 	newPageTab.empty().append(canvasTitle);
- 	
  	// Add both to their respective places in the easel.
  	$("#canvases").append(newCanvas);
- 	$("#page-tabs").append(newPageTab);
  }
  
  // Removes the canvas with the specified id
@@ -91,13 +75,9 @@ function startEasel() {
  	// Delete the canvas
  	$("#" + canvasId).remove();
  	
- 	// Remove the associated pageTab
- 	pageTabId = "#pagetab-" + numberFromIdString(canvasId);
+ 	// Remove the associated pageTabs from any nav elements
+ 	removeTabFromNavElements(canvasId);
  	
- 	$(pageTabId).remove();
- 	
- 	// Since that was the active canvas we need to switch what canvas is active
- 	// And select the associated pageTab
  	if (wasActiveCanvas) {
  		var canvases = $("#canvases").children(".canvas");
  		
@@ -106,8 +86,8 @@ function startEasel() {
  			// Make the first one active
  			$(canvases[0]).addClass("active");
  			
- 			// Highlight the appropriate pageTab
- 			$("#pagetab-" + numberFromIdString(canvases[0].id)).addClass("selected");
+ 			// Make the corresponding page button active as well
+ 			$("#pagebutton-" + numberFromIdString(canvases[0].id)).addClass("canvas-visible");
  		}
  	}
  }
@@ -115,38 +95,6 @@ function startEasel() {
 /* Routes clicks on canvas elements */
 function canvasWasClicked(canvas) {
 	console.log("canvasWasClicked");
-}
-
-
-/* Routes page-tab clicks */
-function pageTabWasClicked(pageTab) {
-	console.log("pageTabWasClicked");
-	
-	if ($(pageTab).hasClass("selected")) {
-		// Do nothing. Canvas should not change
-	} else {
-		// Deselect old tab
-		$(".page-tab.selected").removeClass("selected");
-		
-		// Select new tab
-		$(pageTab).addClass("selected");
-		
-		// get page tab number
-		
-		// get canvasId of new canvas
-		var canvasId = "canvas-" + numberFromIdString($(pageTab).attr('id'));
-		
-		// call canvasWasChangeTo and give it canvasId
-		canvasWasChangedTo(canvasId);
-		
-		// Update the correct pageButton to be shown as visible
-		var pageButtonId = "pagebutton-" + numberFromIdString($(pageTab).attr('id'));
-		
-		// Remove canvas-visible from all page buttons, add just to visible one
-		$(".page-button").removeClass("canvas-visible");
-		
-		$("#" + pageButtonId).addClass("canvas-visible");
-	}
 }
 
 /* Hides all canvases except that with id = canvasId */
@@ -228,22 +176,96 @@ function elementSelectionWasChangedTo(elementId) {
  *	Element creation methods
  */
 function addNewElementofTypeToActiveCanvas(type) {
-	// Increment element counter
+	// If it is a nav-element, run addNavElementToActiveCanvas()
+	if (type === 'nav-element') {
+		addNavElementToActiveCanvas();
+	} else {
+		// Otherwise directly call and append newElementOfType()
+		var newElement = newElementOfType(type);
+	
+		// Append the element to the active canvas
+		$(".canvas.active").append(newElement);
+	}
+}
+
+/*
+ *	This special method controls the creation of the singleton nav element 
+ *  (singleton relative to each canvas)
+ */
+function addNavElementToActiveCanvas() {
+	// First check if the canvas already has a nav element. If it doesn't proceed.
+	if ($(".canvas.active").children(".nav-element").length == 0) {
+		// Get a new nav element
+		var navElement = newElementOfType("nav-element");
+		
+		// For each current page, get the title and canvas id and make a newPageTab to append to navElement
+		var titles = [];
+		var ids = [];
+		
+		$(".page-button").each( function() {
+			// Exclude the add new button
+			if ($(this).attr('id') != 'add-new-page' && $(this).attr('id') != 'page-button-template') {
+				titles.push($(this).children(".page-button-title").val());
+				ids.push($(this).attr('id')); 
+			}
+		});
+		
+		// Now make each page button and append to the navElement
+		for (var i = 0; i < ids.length; i++) {
+			navElement.append(newPageTab(ids[i], titles[i]));
+		}
+		
+		// Set the page tab that corresponds to this canvas to be "selected"
+		var activeCanvasId = $(".canvas.active").attr('id');
+		
+		activeCanvasId = numberFromIdString(activeCanvasId);
+		
+		var idClass = "pagetab-" + activeCanvasId;
+		
+		navElement.children("." + idClass).addClass("selected");
+		
+		// Now append the nav Element to the canvas
+		$(".canvas.active").append(navElement);
+	} else {
+		alert("Already have nav");
+	}
+}
+
+function newElementOfType(type) {
 	elementCounter += 1;
 	
 	// Get appropriate element template
 	var templateId = "#" + type + "-template";
-	
+
 	// Clone the element
 	var newElement = $(templateId).clone(true).removeAttr('id');
-	
+
 	// Set the id
 	var newElementId = "el-" + elementCounter;
-	
+
 	newElement.attr('id', newElementId);
 	
-	// Append the element to the active canvas
-	$(".canvas.active").append(newElement);
+	return newElement;
+}
+
+function newPageTab(id, title) {
+	console.log("newPageTab");
+	console.log("id is: " + id + " title is: " + title);
+	
+	var newPageTab = $("#page-tab-template").clone().removeAttr('id');
+	
+	// Set the title of the page tab
+	newPageTab.empty();
+	
+	newPageTab.append(title);
+
+	// Set the href of the page tab
+	var idClass = "pagetab-" + numberFromIdString(id);
+	
+	newPageTab.addClass(idClass);
+	
+	// Return the new page tab
+	return newPageTab;	
 }
 
 /*
@@ -260,6 +282,41 @@ function deleteElementWasClicked(deleteControl) {
 		
 		$(deleteControl).parent().addClass("delete");
 	}
+}
+
+/* nav-element methods */
+function removeTabFromNavElements(canvasId) {
+	// Need to remove all corresponding tabs from any existing nav elements
+	$("#canvases").find(".nav-element").each( function() {
+		$(this).children(".pagetab-" + numberFromIdString(canvasId)).remove();
+	});
+}
+
+function updatePageTabTitleForId(pageId, title) {
+	$("#canvases").find(".nav-element").children(".pagetab-" + numberFromIdString(pageId)).empty().append(title);
+}
+
+function updateNavElementsWithNewPageTab(pageButtonId, title) {	
+	// Need to add new page tab for new page
+	$("#canvases").find(".nav-element").each( function() {		
+		
+		var pageTab = newPageTab(pageButtonId, title);
+				
+		// Check to see if this is the canvas of this page tab. If it is, tab should be selected.
+		var canvasId = $(this).parent().attr('id');
+		
+		console.log("canvasId is: " + canvasId);
+		
+		canvasId = numberFromIdString(canvasId);
+		pageButtonId = numberFromIdString(pageButtonId);
+		
+		if (pageButtonId == canvasId) {
+			pageTab.addClass("selected");
+		}
+		
+		// Now append the page tab to the nav element
+		$(this).append(pageTab);
+	});
 }
 
 // Takes string of format name-# and returns #
--- js/editor.js
+++ js/editor.js
@@ -30,6 +30,11 @@ function startEditor() {
 	$("#add-new-page").click( function() {
 		addPageButtonWasClicked();
 	})
+	
+	// Add click handler for the page buttons
+	$(".page-button").not("#add-new-page").click( function() {
+		pageButtonWasClicked(this);
+	});
 }
 
 // Templates Section Methods
@@ -40,6 +45,58 @@ function pageButtonHover() {
 	}, function() {
 		$(this).removeClass("hovered");
 	});
+}
+
+function pageButtonWasClicked(pageButton) {
+	// Check if this is the first click on the pageButton
+	if ($(pageButton).hasClass("canvas-visible")) {
+		// Not the first click
+	} else {
+		// Remove canvas-visible, active, delete and edit from all other page buttons
+		$(".page-button").not(pageButton).removeClass("canvas-visible active delete edit");
+		// Add it to this one
+		$(pageButton).addClass("canvas-visible");
+		
+		// Make the corresponding canvas visible
+		canvasWasChangedTo("canvas-" + numberFromIdString(pageButton.id));
+	}
+}
+
+/* Edit control methods */
+function pageButtonEditWasClicked(editControl) {
+	if ($(editControl).parent().hasClass("edit")) {
+		var pageButtonTitle = $(editControl).parent().children(".page-button-title");
+		
+		// Make sure the new title has length
+		if (pageButtonTitle.val().length > 0) {
+			// Done editing, lock the text input
+			pageButtonTitle.attr('readonly', true);
+		
+			// Remove the active and edit states
+			$(editControl).parent().removeClass("active edit");
+			
+			// Update all the nav elements
+			updatePageTabTitleForId($(editControl).parent().attr('id'), pageButtonTitle.val());
+		} else {
+			// Not a valid title, refocus
+			pageButtonTitle.focus();
+		}
+	} else {
+		// Remove active, edit and delete from all other page buttons
+		$(".page-button").removeClass("active edit delete");
+		
+		// First click. Add the edit active classes to the page button
+		$(editControl).parent().addClass("edit active");		
+		
+		// Manipulate page button title 
+		var pageButtonTitle = $(editControl).parent().children(".page-button-title");
+		
+		// Make the page button title editable
+		pageButtonTitle.removeAttr('readonly');
+		
+		// Focus on the page button title
+		pageButtonTitle.focus().select();
+	}
 }
 
 /* Delete Control Methods */
@@ -136,211 +193,22 @@ function addNewPageWithTitle(title) {
 	// Set the value of the text input to the title
 	newPageButton.children(".page-button-title").val(title);
 	
+	console.log("newPageId: " + newPageId);
+	
 	// Clear the add new page title
 	$("#add-new-page").children(".page-button-title").val("");
 	
 	// Add the page button before the add new page button
 	$("#add-new-page").before(newPageButton);
 	
-	// Set the new page to be active everywhere via pageTabWasClicked();
-	pageTabWasClicked($("#pagetab-" + canvasCounter));
+	// Set the new page to be active canvas and active page-button everywhere via pageTabWasClicked();
+	// DO THIS	
 	
 	$("#add-new-page").addClass("second");
+	
+	// Update any existing nav elements
+	updateNavElementsWithNewPageTab(newPageId, title);
 }
-// 
-// function pageButtonHover() {
-// 	$(".page-button").hover( function() {
-// 		console.log("Hovered over page-button");
-// 
-// 		$(this).addClass("is-hovered");
-// 
-// 		// If it's the new page button
-// 		if (this.id === 'add-new-page') {
-// 			// Check if the add new title is already being edited. If it isn't, change the
-// 			// add icon and add a click handler for the button
-// 			if ($(this).children(".page-button-title").attr('readonly')) {
-// 				// Change add icon
-// 				$(this).children(".add").addClass("hovered");
-// 
-// 				$(this).click( function() {
-// 					$(this).unbind("click");
-// 					addNewPageButtonWasClicked(this);
-// 				});
-// 			}
-// 		} else {
-// 			// Grab controls
-// 			var deleteControl = $(this).children(".delete");
-// 			var editControl = $(this).children(".edit");
-// 			var pageButtonTitle = $(this).children(".page-button-title");
-// 		
-// 			// Show controls
-// 			deleteControl.fadeIn(300);
-// 			editControl.fadeIn(300);
-// 		
-// 			// Assign click handlers
-// 			deleteControl.click( function() {
-// 				$(this).unbind("click");
-// 				pageButtonDelete(this)
-// 			});
-// 			editControl.click( function() {
-// 				$(this).unbind("click");
-// 				pageButtonEditWasClicked(pageButtonTitle)
-// 			});
-// 		}
-// 	}, function() {
-// 		console.log("Left page-button");
-// 		
-// 		$(this).removeClass("is-hovered");
-// 		
-// 		if (this.id === 'add-new-page') {
-// 			$(this).children(".add").removeClass("hovered");
-// 		} else {
-// 			// Grab controls
-// 			var deleteControl = $(this).children(".delete");
-// 			var editControl = $(this).children(".edit");
-// 			var pageButtonTitle = $(this).children(".page-button-title");
-// 		
-// 			// Unbind handlers
-// 			deleteControl.unbind("click");
-// 			editControl.unbind("click");
-// 			
-// 			// Only fade edit control if they aren't active
-// 			if (!editControl.hasClass("editing")) {
-// 				editControl.fadeOut(200);
-// 				
-// 				if (!deleteControl.hasClass("deleting")) {
-// 					deleteControl.fadeOut(200);
-// 				}
-// 			}
-// 		}
-// 	});
-// }
-// 
-// function pageButtonEditWasClicked(pageButtonTitle) {
-// 	console.log("Edit Clicked");
-// 	
-// 	if ($(pageButtonTitle).attr('readonly')) {
-// 		// Highlight the edit button
-// 		$(pageButtonTitle).parent().children(".edit").addClass("editing");
-// 				
-// 		// Page button title was not editable. Call focus on pageButtonTitle to edit.
-// 		$(pageButtonTitle).focus();
-// 		
-// 		$(pageButtonTitle).select();
-// 				
-// 		$(pageButtonTitle).removeAttr('readonly');
-// 		
-// 		// Add handler for ways editing can end. All should call pageButtonDidFinishEditing().		
-// 		$(pageButtonTitle).parent().children(".edit").click( function() {
-// 			$(this).unbind("click");
-// 			pageButtonDidFinishUpdating(pageButtonTitle);
-// 		});
-// 		
-// 		$(pageButtonTitle).keypress( function(event) {
-// 			if (event.which == 13) {
-// 				$(this).unbind("keypress");
-// 				pageButtonDidFinishUpdating(pageButtonTitle);
-// 				
-// 				// Check if mouse if over the pageButton and hide the controls if it isn't
-// 				if (!$(pageButtonTitle).parent().hasClass("is-hovered")) {
-// 					$(pageButtonTitle).parent().children(".delete").fadeOut(200);
-// 					$(pageButtonTitle).parent().children(".edit").fadeOut(200);
-// 				}
-// 			}
-// 		});		
-// 	}
-// }
-// 
-// function pageButtonDidFinishUpdating(pageButtonTitle) {
-// 	console.log("pageButtonDidFinishUpdating");
-// 	
-// 	// Lock the pageButtonTitle from editing.
-// 	$(pageButtonTitle).attr('readonly', true);
-// 	
-// 	// Update the page template name.
-// 	updatePageTemplate(pageButtonTitle);
-// 	
-// 	// If the pagebutton is an existing page button rebind and dehighlight
-// 	if ($(this).parent().attr('id') != 'add-new-page') {
-// 		// Remove the highlight on the edit button
-// 		$(pageButtonTitle).parent().children(".edit").removeClass("editing");
-// 	
-// 		// Edit click needs to be rebound to look for further edits
-// 		$(pageButtonTitle).parent().children(".edit").click( function() {
-// 			pageButtonEditWasClicked(pageButtonTitle);
-// 		});
-// 	}
-// }
-// 
-// function addNewPageButtonWasClicked(addNewButton) {
-// 	// Grab addNewTitle
-// 	var addNewTitle = $(addNewButton).children(".page-button-title");
-// 	
-// 	// Add new was clicked. Remove readonly from page button title to allow editing.
-// 	$(addNewTitle).removeAttr('readonly');
-// 	
-// 	// Focus on the add new title
-// 	$(addNewTitle).focus();
-// 	
-// 	// Remove class hovered from add button
-// 	$(addNewButton).children(".add").removeClass("hovered");
-// 	
-// 	// Add click handler to add button. When it detects a click it calls pageButtonDidFinishUpdating
-// 	// to lock the new page name.
-// 	$(addNewButton).children(".add").click( function() {
-// 		$(this).unbind("click");
-// 		
-// 		pageButtonDidFinishUpdating(addNewTitle);
-// 	});
-// 	
-// 	// Also accept enter as an add action
-// 	$(addNewTitle).keypress( function(event) {
-// 		if (event.which == 13) {
-// 			$(this).unbind("keypress");
-// 			pageButtonDidFinishUpdating(addNewTitle);
-// 		}
-// 	});		
-// }
-// 
-// function pageButtonDelete(deleteControl) {
-// 	console.log("Delete Clicked");
-// }
-// 
-// function updatePageTemplate(pageButtonTitle) {
-// 	console.log("updatePageTemplate was called");
-// 	
-// 	if ($(pageButtonTitle).parent().attr('id') === 'add-new-page') {
-// 		console.log("Is new page button");
-// 		
-// 		newPageButtonShouldBeAdded(pageButtonTitle);
-// 	} else {
-// 		console.log("Is page name update");
-// 	}
-// }
-// 
-// function newPageButtonShouldBeAdded(pageButtonTitle) {
-// 	console.log("New page button should be added");
-// 	
-// 	// Clone the pageButtonTemplate
-// 	var newPageButton = $("#page-button-template").clone(true, true).removeAttr('id');
-// 	
-// 	// Set the placeholder of the newPageButton to that of pageButtonTitle (the value stored
-// 	// in the addNewPageButton).	
-// 	newPageButton.children(".page-button-title").val($(pageButtonTitle).val());	
-// 
-// 	// Remove the value of the add new page title
-// 	$(pageButtonTitle).val("");
-// 	
-// 	// Unbind the enter watcher as it has served its purpose.
-// 	$(pageButtonTitle).unbind("keypress");
-// 	
-// 	// Unbind the click watcher on the add button
-// 	$(pageButtonTitle).parent().children(".add").unbind("click");
-// 	
-// 	// Append the new button to the DOM before the add new page button
-// 	$(pageButtonTitle).parent().before(newPageButton);
-// 	
-// }
 
 // Elements Section Methods
 function draggableElements() {
